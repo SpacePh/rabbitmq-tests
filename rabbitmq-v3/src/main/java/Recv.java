7eb00cdaccbb50c83149f9c3eff7aac159ab3613
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-11-02 17:13:04 +0000

    Initial commit

---
+++ rabbitmq-v3/src/main/java/Recv.java
@@ -0,0 +1,31 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class Recv {
+
+    private final static String QUEUE_NAME = "hello_v3";
+
+    public static void main(String[] args) throws Exception {
+        // Creating connection to server
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // Declare queue and publish message
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // Buffering the messages until we're ready to use them
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        while(true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            System.out.println(" [x] Received '" + message + "'");
+        }
+    }
+}
---
+++ rabbitmq-v3/src/main/java/Send.java
@@ -0,0 +1,27 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello_v3";
+
+    public static void main(String[] args) throws Exception {
+
+        // Creating connection to server
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // Declare queue and publish message
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        // Closing channel and connection
+        channel.close();
+        connection.close();
+    }
+}
